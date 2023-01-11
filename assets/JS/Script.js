8363b6b9421a09cf9737a310c36d385c38d21901
AxelPanic <alexthickey@gmail.com>
2023-01-10 21:22:18 -0600

    wiki attempt 1

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,7 +1,8 @@
 // Project by Sam, JJ, and Alex
 var inputEl = document.querySelector('#search-input');
+//var a = "alpha"
 
-document.getElementById("butt").addEventListener("click", formSubmitEvent);
+document.getElementById("button").addEventListener("click", formSubmitEvent);
 
 //function displaySearch() {
 // document.getElementById("#search-input") = inputValue;
@@ -13,14 +14,15 @@ function formSubmitEvent(event) {
     event.preventDefault();
 
     var inputValue = document.querySelector('#search-input').value;
+    let wikiUrl = 'https//api.wikimedia.org/core/v1/wikipedia/en/search/title?q=' + inputValue + "&limit=5";
 
-
-    // cHANGE TO MODAl
+    // CHANGE TO MODAl
   if (!inputValue) {
     alert('Must search a movie');
     return;
   }
 searchApi(inputValue)
+wikiApi(wikiUrl)
 }
 
 function searchApi(inputValue) {
@@ -31,10 +33,25 @@ function searchApi(inputValue) {
 
     fetch ('http://www.omdbapi.com/?t='+ inputValue + "&apikey=639261e3")
         .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        
-        })
-
-        
+        .then(data => { a = data;
+            console.log(a)
+            })
+       
 }
+
+//Wiki api key
+//Client ID: 5d8f2b1c6489f22db36ff531382c134c
+//Client secret: d3e70f33c20e1aca08797c50620b465a0feb8b2e
+
+function wikiApi(wikiUrl) {
+  let response = fetch( wikiUrl,
+    {
+        headers: {
+            'Authorization': '5d8f2b1c6489f22db36ff531382c134c d3e70f33c20e1aca08797c50620b465a0feb8b2e',
+            'Api-User-Agent': 'Project movei finder'
+        }
+    }
+);
+response.json()
+    .then(console.log).catch(console.error);
+  }
