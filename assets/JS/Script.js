70e9999c3fd9b15ee252cdaaa2a3a77063825c21
AxelPanic <alexthickey@gmail.com>
2023-01-17 18:31:41 -0600

    adding imdb as wiki denied application

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,6 +1,9 @@
 // Project by Sam, JJ, and Alex
 var inputEl = document.querySelector('#search-input');
-//var a = "alpha"
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
 
 document.getElementById("button").addEventListener("click", formSubmitEvent);
 
@@ -13,45 +16,39 @@ document.getElementById("button").addEventListener("click", formSubmitEvent);
 function formSubmitEvent(event) {
     event.preventDefault();
 
-    var inputValue = document.querySelector('#search-input').value;
-    let wikiUrl = 'https//api.wikimedia.org/core/v1/wikipedia/en/search/title?q=' + inputValue + "&limit=5";
-
+    const inputValue = document.querySelector('#search-input').value;
+    
     // CHANGE TO MODAl
   if (!inputValue) {
     alert('Must search a movie');
     return;
   }
 searchApi(inputValue)
-wikiApi(wikiUrl)
+
 }
 
 function searchApi(inputValue) {
     console.log(inputValue);
-    
+    var imdbId = ""
     // Generated API key
     // http://www.omdbapi.com/?i=tt3896198&apikey=639261e3
 
     fetch ('http://www.omdbapi.com/?t='+ inputValue + "&apikey=639261e3")
-        .then(response => response.json())
-        .then(data => { a = data;
-            console.log(a)
+        .then((response) => response.json())
+        .then((data) => { a = data;
+            console.log(a);
+            var imdbId = a.imdbID;
+            console.log(imdbId);
+            fetch('https://imdb-api.com/en/API/Title/k_u1amb416/' + imdbId, requestOptions)
+              .then(response => response.json())
+              .then(result => console.log(result))
+              
             })
-       
+            
+    
+  .catch(error => console.log('error', error));
 }
 
-//Wiki api key
-//Client ID: 5d8f2b1c6489f22db36ff531382c134c
-//Client secret: d3e70f33c20e1aca08797c50620b465a0feb8b2e
+//imdb api key = k_u1amb416 (limit 1k a day)
 
-function wikiApi(wikiUrl) {
-  let response = fetch( wikiUrl,
-    {
-        headers: {
-            'Authorization': '5d8f2b1c6489f22db36ff531382c134c d3e70f33c20e1aca08797c50620b465a0feb8b2e',
-            'Api-User-Agent': 'Project movei finder'
-        }
-    }
-);
-response.json()
-    .then(console.log).catch(console.error);
-  }
+
